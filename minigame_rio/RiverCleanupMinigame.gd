8e2e063e9faa75a7ea9917c85d73ce121a56fcad
Dylan11092005 <dysegurase@est.utn.ac.cr>
2026-06-07 12:29:44 -0600

    Estandarizacion del diseño del tiempo y pantallas de ganador y perdedor

--- minigame_rio/RiverCleanupMinigame.gd
+++ minigame_rio/RiverCleanupMinigame.gd
@@ -1,45 +1,51 @@
 extends Node2D
 
-@export var time_limit := 30
+@export var time_limit := 30.0
 @export var drop_distance := 100.0
 
 var trash_total := 0
 var trash_collected := 0
-var game_finished := false
+var game_active := false
+
+const TIMER_HUD_SCENE = preload("res://ui_global/timer_ui.tscn")
+const PANEL_RESULTADO_SCENE = preload("res://ui_global/ResultadoJuego.tscn")
+
+var timer_hud: CanvasLayer
+var panel_resultado: CanvasLayer
 
 @onready var basurero = $Basurero
 @onready var basuras = $Basuras
-@onready var game_timer = $GameTimer
-@onready var timer_label = $CanvasLayer/TimerLabel
-@onready var result_label = $CanvasLayer/ResultLabel
-@onready var back_button = $CanvasLayer/BackButton
+@onready var btn_back = $CanvasLayer/BackButton
 
 
 func _ready():
-	result_label.visible = false
+	timer_hud = TIMER_HUD_SCENE.instantiate()
+	add_child(timer_hud)
+
+	timer_hud.tiempo_agotado.connect(_on_tiempo_agotado)
+	timer_hud.set_tamano_panel(500, 60)
+
+	panel_resultado = PANEL_RESULTADO_SCENE.instantiate()
+	add_child(panel_resultado)
+
+	btn_back.pressed.connect(_on_back_pressed)
 
 	trash_total = basuras.get_child_count()
+	trash_collected = 0
 
 	for trash in basuras.get_children():
 		trash.dropped.connect(_on_trash_dropped)
 
-	game_timer.wait_time = time_limit
-	game_timer.one_shot = true
-	game_timer.timeout.connect(_on_game_timer_timeout)
-	game_timer.start()
-
-	back_button.pressed.connect(_on_back_button_pressed)
+	_start_game()
 
 
-func _process(delta):
-	if game_finished:
-		return
-
-	timer_label.text = "Tiempo: " + str(ceil(game_timer.time_left))
+func _start_game():
+	game_active = true
+	timer_hud.iniciar(time_limit, "Tiempo restante", "para limpiar el río")
 
 
 func _on_trash_dropped(trash):
-	if game_finished:
+	if not game_active:
 		return
 
 	var distance_to_bin = trash.global_position.distance_to(basurero.global_position)
@@ -49,45 +55,27 @@ func _on_trash_dropped(trash):
 		trash.queue_free()
 
 		if trash_collected >= trash_total:
-			_win_game()
+			_win()
 	else:
 		trash.return_to_start()
 
 
-func _win_game():
-	game_finished = true
-	game_timer.stop()
-
-	result_label.text = "¡GANASTE!\nEl río quedó limpio"
-	result_label.visible = true
-	result_label.scale = Vector2(0.3, 0.3)
-	result_label.modulate = Color(1, 1, 1, 0)
-
-	var tween = create_tween()
-	tween.set_parallel(true)
-	tween.tween_property(result_label, "scale", Vector2(1.2, 1.2), 0.35).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	tween.tween_property(result_label, "modulate", Color(1, 1, 1, 1), 0.25)
-
-	tween.set_parallel(false)
-	tween.tween_property(result_label, "scale", Vector2(1.0, 1.0), 0.2)
+func _on_tiempo_agotado():
+	if game_active:
+		_lose()
 
 
-func _on_game_timer_timeout():
-	if trash_collected < trash_total:
-		game_finished = true
-		result_label.text = "¡Tiempo agotado!\nInténtalo de nuevo"
-		result_label.visible = true
-		result_label.scale = Vector2(0.3, 0.3)
-		result_label.modulate = Color(1, 1, 1, 0)
-
-		var tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(result_label, "scale", Vector2(1.1, 1.1), 0.35).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-		tween.tween_property(result_label, "modulate", Color(1, 1, 1, 1), 0.25)
-
-		tween.set_parallel(false)
-		tween.tween_property(result_label, "scale", Vector2(1.0, 1.0), 0.2)
+func _win():
+	game_active = false
+	timer_hud.detener()
+	panel_resultado.mostrar_ganaste()
 
 
-func _on_back_button_pressed():
+func _lose():
+	game_active = false
+	timer_hud.detener()
+	panel_resultado.mostrar_perdiste()
+
+
+func _on_back_pressed():
 	get_tree().change_scene_to_file("res://MenuPrincipal.tscn")
